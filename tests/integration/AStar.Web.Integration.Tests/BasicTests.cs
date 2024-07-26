b4199faa780a969ffa6af297740a1ec7285218b5
Jason Barden <jason.barden@outlook.com>
2024-07-26 21:47:38 +0100

    Add basic test projects and simple Integration tests

---
+++ tests/integration/AStar.Web.Integration.Tests/BasicTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AStar.Web.Integration.Tests;
+
+public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        this.factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/admin")]
+    [InlineData("/counter")]
+    [InlineData("/weather")]
+    public async Task GetEndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        var client = factory.CreateClient();
+
+        var response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+        response.Content.Headers.ContentType.ToString().Should().Be("text/html; charset=utf-8");
+    }
+}
---
+++ tests/unit/AStar.Web.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AStar.Web.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
